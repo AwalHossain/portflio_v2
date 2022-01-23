b9b366104cd139dcc67010814431b9bfbaa194c3
AwalHossain <waqin21@gmail.com>
2022-01-23 22:46:58 +0600

    resumee link updated

--- components/Hero.js
+++ components/Hero.js
@@ -12,7 +12,7 @@ export default function Hero() {
       <div>
         <a
           target="_blank"
-          href="https://docs.google.com/document/d/1wT2sbxX9fiOGCktrnF7f1KyqENfY2rzI/edit?usp=sharing&ouid=100964565921291017617&rtpof=true&sd=true"
+          href="https://docs.google.com/document/d/1wZ7gjalFbLr_c_xZMZahLarcs_DDMImK/edit?usp=sharing&ouid=100964565921291017617&rtpof=true&sd=true"
         >
           <span className="mt-10 inline-block w-auto bg-black font-display text-white text-base md:text-xl py-4 px-7 cursor-pointer">
             <div className="flex flex-row items-center">
